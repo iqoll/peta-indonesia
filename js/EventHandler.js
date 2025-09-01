c52ceddbf1ece4688b729f66c6caf5f57b7a8775
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2025-09-01 10:46:38 +0700

    handleZoom()

--- js/EventHandler.js
+++ js/EventHandler.js
@@ -8,6 +8,7 @@ export default class EventHandlers {
   initEvents() {
     this.handlePathProvinsi();
     this.handleCloseBtn();
+    this.handleZoom();
   }
 
   // Loop ke setiap path svg provinsi
@@ -50,5 +51,56 @@ export default class EventHandlers {
       // Close
       dom.sidePanel.classList.remove("side-panel-open");
     })
+  }
+
+  // Zoom 
+  handleZoom() {
+        let zoomValue = 100;
+    // Disable zoom out on load
+    dom.zoomOutBtn.disabled = true
+
+    // Click event ke zoomIn
+    dom.zoomInBtn.addEventListener("click", () => {
+      // Enable zoom out btn
+      dom.zoomOutBtn.disabled = false;
+      // Increment zoom value 100
+      zoomValue += 100;
+      // Limit zoom adalah 500
+      if(zoomValue < 500) {
+        dom.zoomInBtn.disabled = false;
+      // Jika mencapai limit
+      } else {
+        // Disable zoom in btn
+        dom.zoomInBtn.disabled = true;
+      }
+      // Set map width and height to zoom value
+      dom.peta.style.width = zoomValue + "vw";
+      dom.peta.style.height = zoomValue +"vh";
+
+      // Output zoom value percentage
+      dom.zoomValueOutput.innerText = zoomValue + "%";
+    });
+
+    // Click event ZoomOut
+    dom.zoomOutBtn.addEventListener("click", () => {
+      // Enable zoom out btn
+      dom.zoomInBtn.disabled = false;
+      // Increment zoom value 100
+      zoomValue -= 100;
+      // Limit zoom adalah 100
+      if(zoomValue > 100) {
+        dom.zoomOutBtn.disabled = false;
+      // Jika mencapai limit
+      } else {
+        // Disable zoom in btn
+        dom.zoomOutBtn.disabled = true;
+      }
+      // Set map width and height to zoom value
+      dom.peta.style.width = zoomValue + "vw";
+      dom.peta.style.height = zoomValue +"vh";
+
+      // Output zoom value percentage
+      dom.zoomValueOutput.innerText = zoomValue + "%";
+    });
   }
 }
