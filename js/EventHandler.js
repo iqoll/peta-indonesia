16f53f333d562b0f467c138e3502b1f1b4c82b84
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2025-09-01 10:29:56 +0700

    side-panel, getProvinsiData, close btn event

--- js/EventHandler.js
+++ js/EventHandler.js
@@ -7,6 +7,7 @@ export default class EventHandlers {
 
   initEvents() {
     this.handlePathProvinsi();
+    this.handleCloseBtn();
   }
 
   // Loop ke setiap path svg provinsi
@@ -14,10 +15,40 @@ export default class EventHandlers {
     dom.provinsi.forEach(prov => {
       // Click event
       prov.addEventListener("click", function(e) {
+        // set loading
+        dom.loading.innerText = "Loading..."
+        // Sembunyikan container data provinsi
+        dom.container.classList.add("hide")
+        // show loading
+        dom.loading.classList.remove("hide")
 
+        // variabel nama provinsi
+        let namaProvinsi;
+        // If svg path yang di klik (provinsi) memiliki atribut nama
+        if(e.target.hasAttribute("name")) {
+          // ambil value atribut nama
+          namaProvinsi = e.target.getAttribute("name") 
+        } else {
+          // ambil dari nama class
+          namaProvinsi = e.target.classList.value;
+        }
         // buka side-panel
-        dom.sidePanel.classList.add("side-panel-open")
+        dom.sidePanel.classList.add("side-panel-open");
+
+        // Informasi Provinsi berdasarkan nama provinsi yang di klik
+        getProvinsiData(namaProvinsi).then(data => {
+
+        });
       })
+    })
+  }
+
+  // Close button events
+  handleCloseBtn() {
+    // Click event tutup side-panel
+    dom.closeBtn.addEventListener("click", () => {
+      // Close
+      dom.sidePanel.classList.remove("side-panel-open");
     })
   }
 }
